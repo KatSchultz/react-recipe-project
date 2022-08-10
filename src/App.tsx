1e19a2da903e3fc5d6acd690799adf8af4d473cc
KatSchultz <ks.katieschultz@gmail.com>
2022-08-10 19:13:41 -0400

    App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { Route, Router, Routes } from "react-router-dom";
+import RecipeDisplay from "./components/RecipeDisplay";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe" element={<RecipeDisplay />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

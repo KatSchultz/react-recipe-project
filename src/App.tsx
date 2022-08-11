1d69d8eb233c097d409b7ac1726006fb4c8f3b99
Marlee Garcia <marlee.garcia121@gmail.com>
2022-08-10 21:55:01 -0400

    Tested call without limit

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,48 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import { Route, Router, Routes } from "react-router-dom";
 import RecipeDisplay from "./components/RecipeDisplay";
-import { fetchRandomRecipes, fetchVegetarianRecipes, fetchRecipeById } from "./services/recipes.services";
+import {
+	fetchRandomRecipes,
+	fetchVegetarianRecipes,
+	fetchRecipeById,
+} from "./services/recipes.services";
 import { useEffect, useState } from "react";
 import SearchCriteriaForm from "./components/SearchCriteriaForm";
 import RecipeResults from "./components/RecipeResults";
 
 function App() {
-  const [recipe, setRecipes] = useState();
-  const [recipeId, setRecipeId] = useState('');
+	const [recipe, setRecipes] = useState();
+	const [recipeId, setRecipeId] = useState("");
 
-  useEffect(() => {
-    getRandomRecipe();
-    getVegetartianRecipes();
-    // getRecipeById();
-  }, []);
+	useEffect(() => {
+		getRandomRecipe();
+		getVegetartianRecipes();
+		getRecipeById();
+	}, []);
 
-  function getRandomRecipe() {
-    fetchRandomRecipes().then((response) => console.log(response));
-  }
+	function getRandomRecipe() {
+		fetchRandomRecipes().then((response) => console.log(response));
+	}
 
-  function getVegetartianRecipes() {
-    fetchVegetarianRecipes().then((response) => console.log(response))
-  }
+	function getVegetartianRecipes() {
+		fetchVegetarianRecipes().then((response) => console.log(response));
+	}
 
-  // function getRecipeById() {
-  //   fetchRecipeById(recipeId).then((response) => console.log(response))
-  // }
+	function getRecipeById() {
+		fetchRecipeById(recipeId).then((response) => console.log(response));
+	}
 
-  return (
-    <div className="App">
-      <Header />
-      <SearchCriteriaForm />
+	return (
+		<div className="App">
+			<Header />
+			<SearchCriteriaForm />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe" element={<RecipeResults />} />
-      </Routes>
-    </div>
-  );
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/recipe" element={<RecipeResults />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/services/recipes.services.ts
+++ src/services/recipes.services.ts
@@ -3,19 +3,25 @@ import { config } from "../config";
 import { Recipe, Recipes } from "../types";
 
 export function fetchRandomRecipes() {
-  return axios
-    .get<Recipes[]>(`https://api.spoonacular.com/recipes/complexSearch?sort=random&apiKey=${config.apiKey}`)
-    .then((response) => response.data);
+	return axios
+		.get<Recipes[]>(
+			`https://api.spoonacular.com/recipes/complexSearch?sort=random&apiKey=${config.apiKey}`
+		)
+		.then((response) => response.data);
 }
 
 export function fetchVegetarianRecipes() {
-    return axios
-        .get(`https://api.spoonacular.com/recipes/complexSearch?diet=Vegetarian&apiKey=${config.apiKey}`)
-        .then((response) => response.data)
+	return axios
+		.get(
+			`https://api.spoonacular.com/recipes/complexSearch?diet=Vegetarian&apiKey=${config.apiKey}`
+		)
+		.then((response) => response.data);
 }
 
 export function fetchRecipeById(id: string) {
-    return axios
-        .get<Recipe>(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${config.apiKey}`)
-        .then((response) => response.data)
+	return axios
+		.get<Recipe>(
+			`https://api.spoonacular.com/recipes/645776/information?apiKey=${config.apiKey}`
+		)
+		.then((response) => response.data);
 }

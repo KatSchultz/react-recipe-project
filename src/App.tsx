25ac671fc3d4467666882086b21aaf3f25a08bfb
Marlee Garcia <marlee.garcia121@gmail.com>
2022-08-10 20:06:52 -0400

    Figuring out form

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,30 @@ import { Route, Router, Routes } from "react-router-dom";
 import RecipeDisplay from "./components/RecipeDisplay";
 import { fetchAllRecipes } from "./services/recipes.services";
 import { useEffect, useState } from "react";
+import SearchCriteriaForm from "./components/SearchCriteriaForm";
 
 function App() {
-  const [recipe, setRecipes] = useState();
+	const [recipe, setRecipes] = useState();
 
-  useEffect(() => {
-    getRandomRecipe();
-  }, []);
+	useEffect(() => {
+		getRandomRecipe();
+	}, []);
 
-  function getRandomRecipe() {
-    fetchAllRecipes().then(response => console.log(response.data));
-  }
+	function getRandomRecipe() {
+		fetchAllRecipes().then((response) => console.log(response.data));
+	}
 
-  return (
-    <div className="App">
-      <Header />
+	return (
+		<div className="App">
+			<Header />
+			<SearchCriteriaForm />
 
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe" element={<RecipeDisplay />} />
-      </Routes>
-    </div>
-  );
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/recipe" element={<RecipeDisplay />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/components/SearchCriteriaForm.tsx
+++ src/components/SearchCriteriaForm.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Recipe } from "../types";
 
 export default function SearchCriteriaForm() {
-  return <div>SearchCriteriaForm</div>;
+	const [recipes, setRecipes] = useState<Recipe[]>([{ vegetarian: false }]);
+	const [recipe, setRecipe] = useState<Recipe>({ vegetarian: false });
+	const [active, setActive] = useState(false);
+
+	function handleCheckboxChange(e: React.ChangeEvent<HTMLInputElement>) {
+		console.log(active);
+	}
+
+	return (
+		<div>
+			<form>
+				<label htmlFor="vegetarian">Vegetarian</label>
+				<input
+					type="checkbox"
+					name="vegetarian"
+					id="vegetarian"
+					checked={active}
+					onChange={(e) => setActive(e.target.checked)}
+				/>
+				<button type="submit">Enter</button>
+			</form>
+		</div>
+	);
 }
